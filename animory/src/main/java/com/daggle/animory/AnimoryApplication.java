c63ef7f3c550e4d709a162d4a82437c9712ccd13
latteisacat <dragonldh0907@gmail.com>
2023-09-24 18:24:19 +0900

    feat: #5 프로젝트 생성

---
+++ animory/src/main/java/com/daggle/animory/AnimoryApplication.java
@@ -0,0 +1,13 @@
+package com.daggle.animory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimoryApplication.class, args);
+	}
+
+}
---
+++ animory/src/test/java/com/daggle/animory/AnimoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daggle.animory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
