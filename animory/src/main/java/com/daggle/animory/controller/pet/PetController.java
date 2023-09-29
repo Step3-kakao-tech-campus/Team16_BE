585e1b89e709c7d23b3f646f89661facaa9a0896
latteisacat <dragonldh0907@gmail.com>
2023-09-29 14:36:37 +0900

    fix: 오류 해결을 위해 뭐든 손대보는 중 -> PetFileStorageServiceWithoutSaving.java 삭제

--- animory/src/main/java/com/daggle/animory/controller/pet/PetController.java
+++ animory/src/main/java/com/daggle/animory/controller/pet/PetController.java
@@ -3,7 +3,6 @@ package com.daggle.animory.controller.pet;
 import com.daggle.animory.common.Response;
 import com.daggle.animory.service.pet.PetService;
 import com.daggle.animory.service.pet.fileIO.PetFileStorageService;
-import com.daggle.animory.service.pet.fileIO.PetFileStorageServiceWithoutSaving;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -17,7 +16,6 @@ public class PetController {
 
     private final PetService petService;
     private final PetFileStorageService petFileStorageService;
-    private final PetFileStorageServiceWithoutSaving petFileStorageServiceWithoutSaving;
 
     @PostMapping("/pet")
     public Response<?> registerPet(
@@ -27,18 +25,6 @@ public class PetController {
             ){
         petFileStorageService.storeFile(image);
         petFileStorageService.storeFile(video);
-        System.out.println(PetRequestDTO);
-        Response<?> apiResult = Response.success();
-        return apiResult;
-    }
-    @PostMapping("/pet-test")
-    public Response<?> registerPetWithoutSaving(
-            @RequestPart(value = "key")String PetRequestDTO,
-            @RequestPart(value = "image")MultipartFile image,
-            @RequestPart(value = "video")MultipartFile video
-    ){
-        petFileStorageServiceWithoutSaving.storeFile(image);
-        petFileStorageServiceWithoutSaving.storeFile(video);
         System.out.println(PetRequestDTO);
         Response<?> apiResult = Response.success();
         return apiResult;
