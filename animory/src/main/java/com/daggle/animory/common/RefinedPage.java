dda3c55b93f64321d03b92c6c01cff4c6255fe46
greennarae <smk5647@gmail.com>
2023-10-01 23:11:57 +0900

    fix: RefinedPage hasPrevious/Next 제거

--- animory/src/main/java/com/daggle/animory/common/RefinedPage.java
+++ animory/src/main/java/com/daggle/animory/common/RefinedPage.java
@@ -3,19 +3,19 @@ package com.daggle.animory.common;
 import lombok.Getter;
 import org.springframework.data.domain.Page;
 
+import javax.validation.constraints.Min;
+
 @Getter
 public abstract class RefinedPage {
+
+    @Min(0)
     protected int pageNumber;
     protected int size;
     protected int totalPages;
-    protected boolean hasPrevious;
-    protected boolean hasNext;
 
-    protected RefinedPage(final Page<?> slice) {
-        this.pageNumber = slice.getNumber();
-        this.size = slice.getSize();
-        this.totalPages = slice.getTotalPages();
-        this.hasPrevious = slice.hasPrevious();
-        this.hasNext = slice.hasNext();
+    protected RefinedPage(final Page<?> page) {
+        this.pageNumber = page.getNumber();
+        this.size = page.getSize();
+        this.totalPages = page.getTotalPages();
     }
 }
