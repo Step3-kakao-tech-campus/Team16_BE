4b3a028857a0e00781101a56ca3a89461ac6e7ec
jminkkk <minl741@naver.com>
2023-10-01 20:49:49 +0900

    feat: UserDetails 구현체 생성

---
+++ animory/src/main/java/com/daggle/animory/common/security/UserDetailsImpl.java
@@ -0,0 +1,57 @@
+package com.daggle.animory.common.security;
+
+import com.daggle.animory.domain.account.Account;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+@RequiredArgsConstructor
+@Getter
+
+public class UserDetailsImpl implements UserDetails {
+    private Account account;
+
+    public UserDetailsImpl(Account account) {
+        this.account = account;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<GrantedAuthority> collection = new ArrayList<>();
+        collection.add((GrantedAuthority) () -> account.getRole().toString());
+        return collection;
+    }
+
+    @Override
+    public String getPassword() {
+        return account.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return account.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
