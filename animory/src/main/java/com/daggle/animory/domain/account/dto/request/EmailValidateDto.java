05a7685a735d034219c6b7ca189ac16b46180e48
jminkkk <minl741@naver.com>
2023-10-02 21:46:22 +0900

    style: @Builder을 생성자 메서드에서 레코드단으로 변경

--- animory/src/main/java/com/daggle/animory/domain/account/dto/request/EmailValidateDto.java
+++ animory/src/main/java/com/daggle/animory/domain/account/dto/request/EmailValidateDto.java
@@ -1,9 +1,5 @@
 package com.daggle.animory.domain.account.dto.request;
 
-import java.util.Objects;
 
 public record EmailValidateDto(String email) {
-    public EmailValidateDto {
-        Objects.requireNonNull(email);
-    }
 }
