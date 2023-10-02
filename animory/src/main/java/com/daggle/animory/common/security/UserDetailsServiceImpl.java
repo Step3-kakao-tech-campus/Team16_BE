2781cc8b590ba9182b0811a963e8c17da5b80e42
jminkkk <minl741@naver.com>
2023-10-02 21:23:24 +0900

    fix: UserDetailsServiceImpl에 @Service 어노테이션 추가

--- animory/src/main/java/com/daggle/animory/common/security/UserDetailsServiceImpl.java
+++ animory/src/main/java/com/daggle/animory/common/security/UserDetailsServiceImpl.java
@@ -5,7 +5,9 @@ import com.daggle.animory.domain.account.AccountRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserDetailsServiceImpl implements UserDetailsService {
     private AccountRepository accountRepository;
 
