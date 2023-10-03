e687fc8ba11b635dd797b0455576f529e1d328d8
Moly <102847513+jminkkk@users.noreply.github.com>
2023-10-03 16:34:34 +0900

    feat: 페이지 최소값 검증 추가

--- animory/src/main/java/com/daggle/animory/domain/shelter/ShelterController.java
+++ animory/src/main/java/com/daggle/animory/domain/shelter/ShelterController.java
@@ -3,8 +3,11 @@ package com.daggle.animory.domain.shelter;
 import com.daggle.animory.common.Response;
 import com.daggle.animory.domain.shelter.dto.response.ShelterProfilePage;
 import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.constraints.Min;
+@Validated
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/shelter")
@@ -12,7 +15,7 @@ public class ShelterController {
     private final ShelterService shelterService;
 
     @GetMapping("/{shelterId}")
-    public Response<ShelterProfilePage> getShelter(@PathVariable Long shelterId, @RequestParam("page") final int page) {
+    public Response<ShelterProfilePage> getShelter(@PathVariable @Min(0) Integer shelterId, @RequestParam("page") @Min(0) final int page) {
         return Response.success(shelterService.getShelterProfile(shelterId, page));
     }
 }
