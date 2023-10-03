8c0e72a8048d3b9486350e0b46109239f1b13b7c
Moly <102847513+jminkkk@users.noreply.github.com>
2023-10-03 23:52:11 +0900

    feat: 이메일 중복 검증 로직

--- animory/src/main/java/com/daggle/animory/domain/account/AccountService.java
+++ animory/src/main/java/com/daggle/animory/domain/account/AccountService.java
@@ -1,14 +1,19 @@
 package com.daggle.animory.domain.account;
 
+import com.daggle.animory.common.error.exception.BadRequest400;
 import com.daggle.animory.domain.account.dto.request.EmailValidateDto;
 import com.daggle.animory.domain.account.dto.request.AccountLoginDto;
 import com.daggle.animory.domain.account.dto.request.ShelterSignUpDto;
 import com.daggle.animory.domain.account.dto.response.AccountLoginSuccessDto;
+import lombok.RequiredArgsConstructor;
 import org.apache.commons.lang3.NotImplementedException;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class AccountService {
+    private final AccountRepository accountRepository;
+
     public void registerShelterAccount(ShelterSignUpDto shelterSignUpDto) {
         throw new NotImplementedException("NotImplemented yet");
     }
@@ -18,6 +23,7 @@ public class AccountService {
     }
 
     public void validateEmailDuplication(EmailValidateDto emailValidateDto) {
-        throw new NotImplementedException("NotImplemented yet");
+        if (accountRepository.existsByEmail(emailValidateDto.email()))
+            throw new BadRequest400("존재하는 이메일입니다.");
     }
 }
