137b70d8b32be72a6f61bda49d65e4a2ed0955db
jminkkk <minl741@naver.com>
2023-10-03 13:36:31 +0900

    feat: Shelter 빈 컨트롤러 생성

---
+++ animory/src/main/java/com/daggle/animory/domain/shelter/ShelterController.java
@@ -0,0 +1,18 @@
+package com.daggle.animory.domain.shelter;
+
+import com.daggle.animory.common.Response;
+import com.daggle.animory.domain.shelter.dto.response.ShelterProfilePage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/shelter")
+public class ShelterController {
+    private final ShelterService shelterService;
+
+    @GetMapping("/shelter/{shelterId}")
+    public Response<ShelterProfilePage> getShelter(@PathVariable Long shelterId, @RequestParam("page") final int page) {
+        return Response.success(shelterService.getShelterProfile(shelterId, page));
+    }
+}
