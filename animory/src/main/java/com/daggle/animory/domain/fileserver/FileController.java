e9340bee382ac14260c3576a8157a56e312cf7c8
latteisacat <dragonldh0907@gmail.com>
2023-10-03 19:05:23 +0900

    fix: 구현체 변경에 따른 기존 작업 코드 수정

--- animory/src/main/java/com/daggle/animory/domain/fileserver/FileController.java
+++ animory/src/main/java/com/daggle/animory/domain/fileserver/FileController.java
@@ -1,11 +1,21 @@
 package com.daggle.animory.domain.fileserver;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
+import org.springframework.http.ContentDisposition;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,7 +25,19 @@ public class FileController {
     private final FileRepository localFileRepository;
 
     @GetMapping("/file/{fileUrl}")
-    public ResponseEntity<Resource> getFile(@PathVariable final String fileUrl) {
-        return ResponseEntity.ok().body(localFileRepository.getFile(fileUrl));
+    public ResponseEntity<Resource> getFile(@PathVariable final String fileUrl) throws IOException {
+        Path path = Paths.get(fileUrl);
+        String contentType = Files.probeContentType(path);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentDisposition(
+                ContentDisposition.builder("attachment")
+                        .filename(fileUrl, StandardCharsets.UTF_8)
+                        .build());
+        headers.add(HttpHeaders.CONTENT_TYPE, contentType);
+        Resource resource = localFileRepository.getFile(fileUrl);
+
+        return new ResponseEntity<>(resource, headers, HttpStatus.OK);
+
     }
 }
