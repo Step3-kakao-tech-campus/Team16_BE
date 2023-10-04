f2d549acfc3e6ce946d3b4e46e807083a1dc1373
Moly <102847513+jminkkk@users.noreply.github.com>
2023-10-05 01:05:27 +0900

    feat: 보호소에 해당하는 펫 조회 메서드 작성

--- animory/src/main/java/com/daggle/animory/domain/pet/PetRepository.java
+++ animory/src/main/java/com/daggle/animory/domain/pet/PetRepository.java
@@ -3,10 +3,12 @@ package com.daggle.animory.domain.pet;
 import com.daggle.animory.domain.pet.entity.Pet;
 import com.daggle.animory.domain.pet.entity.PetType;
 import com.daggle.animory.domain.shelter.entity.Province;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface PetRepository extends JpaRepository<Pet, Integer> {
 
@@ -15,7 +17,11 @@ public interface PetRepository extends JpaRepository<Pet, Integer> {
         "join fetch p.shelter s " +
         "where s.address.province = :province " +
         "and p.type = :petType")
-    Slice<Pet> findSliceBy(PetType petType, Province province, Pageable searchCondition);
+    Slice<Pet> findSliceBy(@Param("petType") PetType petType, @Param("province") Province province, Pageable searchCondition);
 
     Slice<Pet> findSliceBy(Pageable pageable);
+    @Query("select p " +
+            "from Pet p " +
+            "where p.shelter.id = :shelterId")
+    Page<Pet> findByShelterId(@Param("shelterId")Integer shelterId, Pageable pageable);
 }
