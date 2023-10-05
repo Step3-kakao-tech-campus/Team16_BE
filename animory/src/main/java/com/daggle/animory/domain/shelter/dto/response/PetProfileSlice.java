7e7adf69523e2da9c386d4ed8788f23841b5aa4e
Moly <102847513+jminkkk@users.noreply.github.com>
2023-10-05 11:36:06 +0900

    feat: 보호소 조회 시 등록된 펫이 없을 경우 처리

--- animory/src/main/java/com/daggle/animory/domain/shelter/dto/response/PetProfileSlice.java
+++ animory/src/main/java/com/daggle/animory/domain/shelter/dto/response/PetProfileSlice.java
@@ -2,10 +2,10 @@ package com.daggle.animory.domain.shelter.dto.response;
 
 import com.daggle.animory.common.RefinedPage;
 import com.daggle.animory.domain.pet.entity.Pet;
-import lombok.Builder;
 import lombok.Getter;
 import org.springframework.data.domain.Page;
 
+import java.util.ArrayList;
 import java.util.List;
 @Getter
 public class PetProfileSlice extends RefinedPage {
@@ -15,7 +15,15 @@ public class PetProfileSlice extends RefinedPage {
         super(page);
         this.pets = page.getContent().stream().map(PetProfileDto::of).toList();
     }
+    private PetProfileSlice() {
+        this.pageNumber = 0;
+        this.size = 0;
+        this.totalPages = 0;
+        this.pets = new ArrayList<>();
+    }
+
     public static PetProfileSlice of(Page<Pet> petPage) {
+        if (petPage == null) return new PetProfileSlice();
         return new PetProfileSlice(petPage);
     }
 }
