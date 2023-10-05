195c7861ee9dc65c546ddef348de176c7aa06aec
Moly <102847513+jminkkk@users.noreply.github.com>
2023-10-05 11:35:13 +0900

    feat: 보호소 조회 테스트 코드 작성

---
+++ animory/src/test/java/com/daggle/animory/domain/shelter/ShelterServiceTest.java
@@ -0,0 +1,71 @@
+package com.daggle.animory.domain.shelter;
+
+import com.daggle.animory.domain.pet.PetRepository;
+import com.daggle.animory.domain.pet.entity.Pet;
+import com.daggle.animory.domain.pet.entity.PetType;
+import com.daggle.animory.domain.pet.fixture.PetFixture;
+import com.daggle.animory.domain.shelter.dto.response.ShelterProfilePage;
+import com.daggle.animory.domain.shelter.entity.Shelter;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+
+@ExtendWith(MockitoExtension.class)
+public class ShelterServiceTest  {
+    @InjectMocks
+    private ShelterService shelterService;
+    @Mock
+    private ShelterRepository shelterRepository;
+    @Mock
+    private PetRepository petRepository;
+
+    @Nested
+    class 보호소_조회 {
+        @Test
+        void 성공_보호소_조회() {
+            Shelter shelter = Shelter.builder()
+                    .id(1)
+                    .build();
+
+            Page<Pet> pets = new PageImpl<>(PetFixture.get(5, PetType.DOG, shelter));
+
+            // stub
+            Mockito.when(shelterRepository.findById(any())).thenReturn(Optional.of(shelter));
+            Mockito.when(petRepository.findByShelterId(any(), any())).thenReturn(pets);
+
+            ShelterProfilePage shelterProfilePage = shelterService.getShelterProfile(shelter.getId(), 0);
+
+            assertThat(shelterProfilePage.shelter().id()).isEqualTo(shelter.getId());
+            assertThat(shelterProfilePage.petList().getSize()).isEqualTo(5);
+        }
+
+        @Test
+        void 보호소의_아이들이_없을_경우_빈_배열_리턴() {
+            Shelter shelter = Shelter.builder()
+                    .id(1)
+                    .build();
+
+            // stub
+            Mockito.when(shelterRepository.findById(any())).thenReturn(Optional.of(shelter));
+            Mockito.when(petRepository.findByShelterId(any(), any())).thenReturn(null);
+
+            ShelterProfilePage shelterProfilePage = shelterService.getShelterProfile(shelter.getId(), 0);
+
+            assertThat(shelterProfilePage.shelter().id()).isEqualTo(shelter.getId());
+            assertThat(shelterProfilePage.petList().getPets()).isEmpty();
+        }
+    }
+}
+
