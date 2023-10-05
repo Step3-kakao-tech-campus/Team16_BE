52e1339a331d231274d4c1db22536e127a7c9b7f
greennarae <smk5647@gmail.com>
2023-10-06 02:16:12 +0900

    webMvcConfigurer를 이용한 cors 설정 추가

--- animory/src/main/java/com/daggle/animory/common/config/WebConfiguration.java
+++ animory/src/main/java/com/daggle/animory/common/config/WebConfiguration.java
@@ -2,6 +2,7 @@ package com.daggle.animory.common.config;
 
 import com.daggle.animory.common.logger.RequestLogger;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -16,4 +17,16 @@ public class WebConfiguration implements WebMvcConfigurer {
         registry.addInterceptor(new RequestLogger())
             .order(-1);
     }
+
+    @Override
+    public void addCorsMappings(final CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedHeaders("*")
+            .allowedMethods("*")
+            .allowedOrigins("*")
+            .exposedHeaders("Authorization")
+            .allowCredentials(true);
+    }
+
+
 }
