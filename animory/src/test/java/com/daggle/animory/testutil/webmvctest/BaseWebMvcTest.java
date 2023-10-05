31e3dcad477c67ebfa5091eb5178c37dd7f83e92
greennarae <smk5647@gmail.com>
2023-10-05 13:59:18 +0900

    fix: yml arithmetic expression, webmvctest에서 AutoConfigureMockMvc 제거

--- animory/src/test/java/com/daggle/animory/testutil/webmvctest/BaseWebMvcTest.java
+++ animory/src/test/java/com/daggle/animory/testutil/webmvctest/BaseWebMvcTest.java
@@ -4,7 +4,6 @@ import com.daggle.animory.common.config.SpringSecurityConfiguration;
 import com.daggle.animory.common.security.TokenProvider;
 import com.daggle.animory.common.security.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -13,7 +12,6 @@ import org.springframework.test.web.servlet.MockMvc;
     UserDetailsServiceImpl.class,
     TokenProvider.class
 })
-@AutoConfigureMockMvc
 public abstract class BaseWebMvcTest {
     @Autowired
     protected MockMvc mvc;
