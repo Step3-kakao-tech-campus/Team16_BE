dba02140adbddd21b77f512e7f03fe3aa9d67740
greennarae <smk5647@gmail.com>
2023-10-06 01:54:33 +0900

    fix: account repository webmvctest 의존성 mocking

--- animory/src/test/java/com/daggle/animory/testutil/webmvctest/BaseWebMvcTest.java
+++ animory/src/test/java/com/daggle/animory/testutil/webmvctest/BaseWebMvcTest.java
@@ -3,17 +3,22 @@ package com.daggle.animory.testutil.webmvctest;
 import com.daggle.animory.common.config.SpringSecurityConfiguration;
 import com.daggle.animory.common.security.TokenProvider;
 import com.daggle.animory.common.security.UserDetailsServiceImpl;
+import com.daggle.animory.domain.account.AccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest({
     SpringSecurityConfiguration.class,
     UserDetailsServiceImpl.class,
-    TokenProvider.class
+    TokenProvider.class,
 })
 public abstract class BaseWebMvcTest {
     @Autowired
     protected MockMvc mvc;
+
+    @MockBean
+    protected AccountRepository accountRepository;
 
 }
