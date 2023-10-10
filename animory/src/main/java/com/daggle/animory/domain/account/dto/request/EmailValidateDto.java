dca9420578bd00e3e06286eedb4a375723c6dc55
Moly <102847513+jminkkk@users.noreply.github.com>
2023-10-10 14:05:19 +0900

    feat: 이메일 중복 검증 요청에 null값 및 이메일 형식 검증

--- animory/src/main/java/com/daggle/animory/domain/account/dto/request/EmailValidateDto.java
+++ animory/src/main/java/com/daggle/animory/domain/account/dto/request/EmailValidateDto.java
@@ -1,5 +1,10 @@
 package com.daggle.animory.domain.account.dto.request;
 
 
-public record EmailValidateDto(String email) {
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+
+public record EmailValidateDto(
+        @NotNull(message = "이메일을 입력해주세요.")
+        @Email(message = "이메일 형식에 맞지 않습니다.") String email) {
 }
