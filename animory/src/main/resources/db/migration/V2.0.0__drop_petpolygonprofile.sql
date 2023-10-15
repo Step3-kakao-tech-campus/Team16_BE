5b392bfd3baea108688f2bf46a29fcd64a5dd473
greennarae <smk5647@gmail.com>
2023-10-15 22:59:54 +0900

    chore: H2 실행시 Flyway 비활성화

--- animory/src/main/resources/db/migration/V2.0.0__drop_petpolygonprofile.sql
+++ animory/src/main/resources/db/migration/V2.0.0__drop_petpolygonprofile.sql
@@ -1,12 +1,12 @@
--- PetPolygonProfile을 Embedded 타입으로 변경하고 Pet Entity에서 편리하게 관리하기 위한 수정
+-- PetPolygonProfile을 Embedded 타입으로 변경하고 Pet Entity에서 편리하게 관리하기 위함.
 
 
 -- PET 테이블에 Embedded 타입으로 추가
 ALTER TABLE pet
-    ADD COLUMN activeness   INT NOT NULL,
+    ADD COLUMN activeness INT NOT NULL,
     ADD COLUMN adaptability INT NOT NULL,
-    ADD COLUMN affinity     INT NOT NULL,
-    ADD COLUMN athletic     INT NOT NULL,
+    ADD COLUMN affinity INT NOT NULL,
+    ADD COLUMN athletic INT NOT NULL,
     ADD COLUMN intelligence INT NOT NULL;
 
 -- PET POLYGON PROFILE 테이블 제거
