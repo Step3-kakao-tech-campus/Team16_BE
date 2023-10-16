6665d499bd556f0719c93852fd63da9f886ca7b0
Moly <102847513+jminkkk@users.noreply.github.com>
2023-10-16 13:23:35 +0900

    이메일 빈칸 입력을 막기 위한 @NotBlank 추가

--- animory/src/main/java/com/daggle/animory/domain/account/dto/request/AccountLoginDto.java
+++ animory/src/main/java/com/daggle/animory/domain/account/dto/request/AccountLoginDto.java
@@ -3,11 +3,12 @@ package com.daggle.animory.domain.account.dto.request;
 import lombok.Builder;
 
 import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 @Builder
 public record AccountLoginDto(
         @Email(message = "이메일 형식에 맞지 않습니다.")
-        @NotNull(message = "이메일을 입력해주세요.") String email,
+        @NotBlank(message = "이메일을 입력해주세요.") String email,
         @NotNull(message = "비밀번호를 입력해주세요.") String password) {
 }
--- animory/src/main/java/com/daggle/animory/domain/account/dto/request/EmailValidateDto.java
+++ animory/src/main/java/com/daggle/animory/domain/account/dto/request/EmailValidateDto.java
@@ -2,9 +2,9 @@ package com.daggle.animory.domain.account.dto.request;
 
 
 import javax.validation.constraints.Email;
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.NotBlank;
 
 public record EmailValidateDto(
-        @NotNull(message = "이메일을 입력해주세요.")
+        @NotBlank(message = "이메일을 입력해주세요.")
         @Email(message = "이메일 형식에 맞지 않습니다.") String email) {
 }
