3952fd47656d907bebee9f916d95565ad8755575
greennarae <smk5647@gmail.com>
2023-10-20 00:04:44 +0900

    refactor: S3 File Save method 가독성개선

--- animory/src/main/java/com/daggle/animory/domain/fileserver/S3FileRepository.java
+++ animory/src/main/java/com/daggle/animory/domain/fileserver/S3FileRepository.java
@@ -37,14 +37,14 @@ public class S3FileRepository {
 
     public String save(final MultipartFile file) {
         final String fileName = UUID.randomUUID().toString();
-        final Path path = Paths.get(Objects.requireNonNull(fileName));
+
         try {
-            final String contentType = Files.probeContentType(path);
-            final ObjectMetadata metadata = new ObjectMetadata();
-            metadata.setContentType(contentType);
             amazonS3Client.putObject(
                 new PutObjectRequest(
-                    bucket, fileName, file.getInputStream(), metadata
+                    bucket,
+                    fileName,
+                    file.getInputStream(),
+                    buildObjectMetadata(file, fileName)
                 ).withCannedAcl(CannedAccessControlList.PublicRead));
 
         } catch (final AmazonClientException ex) {
@@ -57,5 +57,23 @@ public class S3FileRepository {
     }
 
     public void deleteAll(final List<String> savedFileUrls) {
+    }
+
+
+
+    private ObjectMetadata buildObjectMetadata(final MultipartFile file,
+                                               final String fileName) throws IOException {
+        final ObjectMetadata metadata = new ObjectMetadata();
+
+        // get content type
+        final Path path = Paths.get(Objects.requireNonNull(fileName));
+        final String contentType = Files.probeContentType(path); // IOException here
+
+        // set metadata
+        metadata.setContentType(contentType);
+        metadata.setContentLength(file.getSize());
+        metadata.setContentType(file.getContentType());
+
+        return metadata;
     }
 }
