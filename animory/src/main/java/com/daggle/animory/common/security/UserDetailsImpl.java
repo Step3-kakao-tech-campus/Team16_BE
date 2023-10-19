8983714b89d437b576cf0397a0462d6abca63994
Moly <102847513+jminkkk@users.noreply.github.com>
2023-10-19 16:21:33 +0900

    feat: 커스텀한 UserDetails 구현

---
+++ animory/src/main/java/com/daggle/animory/common/security/UserDetailsImpl.java
@@ -0,0 +1,51 @@
+package com.daggle.animory.common.security;
+
+import com.daggle.animory.domain.account.entity.Account;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+public class UserDetailsImpl implements UserDetails {
+    private final Account account;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Arrays.stream(account.getRole().toString().split(",")).map(SimpleGrantedAuthority::new).collect(Collectors.toList());
+    }
+
+    @Override
+    public String getPassword() {
+        return account.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return account.getEmail();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
