6e5bc22ce8e5ed5e1fb3a66fb4469451f307c34a
Moly <102847513+jminkkk@users.noreply.github.com>
2023-10-22 00:50:09 +0900

    fix: 예외 메시지 수정

--- animory/src/main/java/com/daggle/animory/common/security/exception/InvalidTokenFormatException.java
+++ animory/src/main/java/com/daggle/animory/common/security/exception/InvalidTokenFormatException.java
@@ -3,6 +3,6 @@ package com.daggle.animory.common.security.exception;
 public class InvalidTokenFormatException extends RuntimeException {
     @Override
     public String getMessage() {
-        return SecurityExceptionMessage.UNAUTHORIZED.getMessage();
+        return SecurityExceptionMessage.INVALID_TOKEN_FORMAT.getMessage();
     }
 }
