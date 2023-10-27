eae0d7815328fff74af43671803a419d5dba5022
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2023-10-27 17:04:35 +0900

    fix: 입양상태 한글 메시지 추가

--- animory/src/main/java/com/daggle/animory/domain/pet/entity/NeutralizationStatus.java
+++ animory/src/main/java/com/daggle/animory/domain/pet/entity/NeutralizationStatus.java
@@ -1,7 +1,17 @@
 package com.daggle.animory.domain.pet.entity;
 
 public enum NeutralizationStatus {
-    YES,
-    NO,
-    UNKNOWN
+    YES("중성화 완료"),
+    NO("중성화 안함"),
+    UNKNOWN("알 수 없음");
+
+    private final String message;
+
+    NeutralizationStatus(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
 }
