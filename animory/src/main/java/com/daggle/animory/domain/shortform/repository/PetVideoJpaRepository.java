6d3cc1cf3022a1c93b20038dfa6e49c1cf6b8b25
greennarae <smk5647@gmail.com>
2023-11-03 01:21:02 +0900

    refactor: 2개의 repository 메소드 정리'

--- animory/src/main/java/com/daggle/animory/domain/shortform/repository/PetVideoJpaRepository.java
+++ animory/src/main/java/com/daggle/animory/domain/shortform/repository/PetVideoJpaRepository.java
@@ -4,12 +4,9 @@ import com.daggle.animory.domain.pet.entity.PetVideo;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 
-import java.util.List;
-
-public interface PetVideoJpaRepository extends JpaRepository<PetVideo, Integer>, JpaSpecificationExecutor<PetVideo> {
+public interface PetVideoJpaRepository extends JpaRepository<PetVideo, Integer> {
 
 
     @Query("""
@@ -17,17 +14,6 @@ public interface PetVideoJpaRepository extends JpaRepository<PetVideo, Integer>,
         from PetVideo pv
         order by pv.likeCount desc
         """)
-    Slice<Integer> findSliceOfIds(Pageable pageable);
-
-    @Query("""
-        select pv
-        from PetVideo pv
-        left join fetch pv.pet p
-        left join fetch p.shelter s
-        where pv.id in :petVideoIds
-        order by pv.likeCount desc
-        """)
-        // WARN: 서비스 전체에 short form video가 하나도 없다면, in 절이 비게 되어서 SQL에러가 발생합니다.
-    List<PetVideo> findAllByPetVideoIdIn(List<Integer> petVideoIds);
+    Slice<Integer> findPetVideoIdsBy(Pageable pageable);
 
 }
--- animory/src/test/java/com/daggle/animory/domain/shortform/repository/PetVideoJpaRepositoryOrderByTest.java
+++ animory/src/test/java/com/daggle/animory/domain/shortform/repository/PetVideoJpaRepositoryOrderByTest.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -18,15 +19,19 @@ import static org.assertj.core.api.Assertions.assertThat;
 @Slf4j
 @Sql("classpath:sql/PetVideoRepositoryOrderByTest.sql")
 @DataJpaTest
+@Import(PetVideoJpqlRepository.class)
 @EnableJpaAuditing
 class PetVideoJpaRepositoryOrderByTest extends WithTimeSupportObjectMapper {
     @Autowired
     private PetVideoJpaRepository petVideoJpaRepository;
 
+    @Autowired
+    private PetVideoJpqlRepository petVideoJpqlRepository;
+
     @Test
     void 홈화면_숏폼조회쿼리_테스트() {
-        Slice<Integer> petVideoIds = petVideoJpaRepository.findSliceOfIds(PageRequest.of(0, 10));
-        List<PetVideo> petVideos = petVideoJpaRepository.findAllByPetVideoIdIn(petVideoIds.getContent());
+        Slice<Integer> petVideoIds = petVideoJpaRepository.findPetVideoIdsBy(PageRequest.of(0, 10));
+        List<PetVideo> petVideos = petVideoJpqlRepository.findAllByIds(petVideoIds.getContent());
         print(petVideos);
 
         assertThat(petVideos.get(0).getLikeCount()).isEqualTo(5000);
