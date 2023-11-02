ce6b114322b9fdd4068aeff9baae24ecaddbf03a
greennarae <smk5647@gmail.com>
2023-11-02 21:54:37 +0900

    fix: 사용하지 않는 코드 제거(petType, province 이름변환)

--- animory/src/main/java/com/daggle/animory/domain/shelter/entity/Province.java
+++ animory/src/main/java/com/daggle/animory/domain/shelter/entity/Province.java
@@ -17,28 +17,6 @@ public enum Province {
     전남,
     경북,
     경남,
-    제주특별자치도
-    ;
+    제주특별자치도;
 
-    public String getProvinceNameForUI() {
-        return switch (this) {
-            case 서울 -> "서울특별시";
-            case 부산 -> "부산광역시";
-            case 대구 -> "대구광역시";
-            case 인천 -> "인천광역시";
-            case 광주 -> "광주광역시";
-            case 대전 -> "대전광역시";
-            case 울산 -> "울산광역시";
-            case 세종특별자치시 -> "세종시";
-            case 경기 -> "경기도";
-            case 강원특별자치도 -> "강원도";
-            case 충북 -> "충청북도";
-            case 충남 -> "충청남도";
-            case 전북 -> "전라북도";
-            case 전남 -> "전라남도";
-            case 경북 -> "경상북도";
-            case 경남 -> "경상남도";
-            case 제주특별자치도 -> "제주도";
-        };
-    }
 }
